0a88a4d02a5dcf21e58d43e9b54ecdc962e61851
shakar <shakarshahedan@gmail.com>
2022-02-09 14:04:17 +0300

    Updated Program.cs

--- src/BabyCareApi/Program.cs
+++ src/BabyCareApi/Program.cs
@@ -1,4 +1,8 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BabyCareApi.Common;
 using BabyCareApi.Extensions;
+using BabyCareApi.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -11,14 +15,28 @@ services
 .ConfigureMongoDB(config)
 .AddEndpointsApiExplorer()
 .AddSwaggerGen()
-;
+.ConfigureAuthentication(config);
+services
+            .AddHttpClient()
+            .ConfigureSwaggerGenerator(config)
+            .AddControllers()
+            .AddJsonOptions(x =>
+            {
+              x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+              x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+
+              x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+services.ConfigureDependencies();
 
 var app = builder.Build();
 app
 .UseSwagger()
 .UseSwaggerUI()
 .UseHttpsRedirection()
-.UseAuthorization();
+.UseAuthorization()
+.UseAuthentication();
 
 app.MapControllers();
 
