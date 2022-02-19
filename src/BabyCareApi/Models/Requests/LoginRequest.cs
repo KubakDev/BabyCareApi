19a8fb8bc76a6aa8b5a0688da0904bdcac1dbe32
shakar <shakarshahedan@gmail.com>
2022-02-19 09:42:26 +0300

    Password is required in login request

--- src/BabyCareApi/Models/Requests/LoginRequest.cs
+++ src/BabyCareApi/Models/Requests/LoginRequest.cs
@@ -9,6 +9,7 @@ public class LoginRequest
   [Required]
   public string Username { get; set; } = string.Empty;
 
+  [Required]
   public string Password { get; set; } = string.Empty;
 
   [Required]
